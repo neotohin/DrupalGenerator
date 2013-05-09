180a6eb5418a798bf681b780c1691983d85409b2
Amzad Hossain <amzad.hossain@sourcetop.com>
2013-05-09 09:20:34 +0600

    Added Sidebar Support

--- DrupalGenerator.py
+++ DrupalGenerator.py
@@ -1,7 +1,16 @@
 import sublime, sublime_plugin
 
-#To Run view.run_command('drupal_generator')
-
+# To Run view.run_command('drupal_generator')
 class DrupalGeneratorCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    self.view.window().show_input_panel('Somthing', 'Default Value', None, None, None)
+  def run(self, edit, dirs):
+    self.view.window().show_input_panel('Module Name:', '', None, None, None)
+
+#Command to Generate Module
+class DrupalGeneratorModuleCommand(sublime_plugin.TextCommand):
+  def run(self, edit, dirs):
+    self.view.window().show_input_panel('Module Name:', dirs[0], None, None, None)
+
+#Command to Generate Theme
+class DrupalGeneratorThemeCommand(sublime_plugin.TextCommand):
+  def run(self, edit, dirs):
+    self.view.window().show_input_panel('Theme Name:', '''', None, None, None)
